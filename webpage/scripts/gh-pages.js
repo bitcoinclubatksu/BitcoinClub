e513f3d9ce27907ac05e94b6d1b597ab6add093f
HarshitJoshi <hjoshi023@gmail.com>
2020-03-10 21:51:25 -0400

    WIP: deployment

---
+++ webpage/scripts/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export',
+    {
+        branch: 'gh-pages',
+        repo: 'git@github.com:HarshitJoshi/BitcoinClub.git',
+        user: {
+            name: 'Harshit Joshi',
+            email: 'hjoshi023@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+);
